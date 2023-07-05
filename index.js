e15c07ab5d2dead69d529f3ee45edb165d8c999e
Nagehan1 <uyumaznagehan@gmail.com>
2023-07-05 13:59:46 +0100

    js file updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const setupTextarea = document.getElementById('setup-textarea') 
-const setupInputContainer = document.getElementById('setup-input-container')
-const movieBossText = document.getElementById('movie-boss-text')
-
-document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-  }
-})
+const apiKey="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"
+const url= "https://api.openai.com/v1/completions"
+fetch(url,{
+	method:'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer ${apiKey}'
+	},
+	body: JSON.stringify({
+		'model': "text-davinci-003",
+		'prompt':"What is the famous singer in Turkey?"
+	})
+}).then(res=>res.json()).then(data =>console.log(data))
